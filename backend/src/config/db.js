8b79e3f12b1b15e9561c62e48a97c539d90c4360
kakaoooo <97776681+kakaoooo@users.noreply.github.com>
2026-06-13 06:00:01 +0000

    fix: update database configuration

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2');
-
-// usecreatePool from .env 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST || process.env.DB_HOST,
+  user: process.env.MYSQLUSER || process.env.DB_USER,
+  password: process.env.MYSQLPASSWORD || process.env.DB_PASS,
+  database: process.env.MYSQLDATABASE || process.env.DB_NAME,
+  port: process.env.MYSQLPORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
